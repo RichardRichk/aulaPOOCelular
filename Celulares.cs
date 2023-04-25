5e31eb185575919dc828edd5e7a3d927a2cac2d1
Richard Fernandes Passarelli <45811737874@info.local>
2023-04-25 11:11:16 -0300

    Finalizando opcao de ligar celular, Falta realizar validacoes

---
+++ Celulares.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aulaPOOCelular
+{
+    public class Celulares
+    {
+        public string Cor;
+        public string Modelo;
+        public char Tamanho;
+
+        public char OpcaoDesligado;
+        public bool Desligado;
+
+        public char OpcaoDesbloqueado;
+        public bool Desbloqueado;
+
+        public char EscolhaLigar;
+
+        public char Funcionalidades;
+
+
+        public char EscolhaLigacao;
+
+        public char EscolhaMensagem;
+
+
+        public void Ligar(){
+            Console.WriteLine($"Ligando...");
+            Thread.Sleep(1250);
+            Console.WriteLine($"Bem vindo!");           
+        }
+
+        public void Desligar(){
+            Console.WriteLine($"Ate logo!");
+            Console.WriteLine($"Desligando...");
+            Thread.Sleep(1250);
+            return;                        
+        }
+
+        public void FazerLigacao(){
+        do
+        {
+            Console.WriteLine
+            (@$"
+            Para quem gostaria de ligar ?
+            [1] - Goku;
+            [2] - Iron man;
+            [3] - Batman; 
+
+            [0] - Desligar Celular;           
+            ");
+            EscolhaLigacao = char.Parse(Console.ReadLine());
+            switch (EscolhaLigacao)
+            {
+                case '1':
+                Console.WriteLine($"Ligando para Goku...");
+                Thread.Sleep(1250);
+                Console.WriteLine($"Oi, Eu sou o Goku!"); 
+                Console.ReadLine();              
+                    break;
+
+                case '2':
+                Console.WriteLine($"Ligando para Iron man...");
+                Thread.Sleep(1250);
+                Console.WriteLine($"E eu sou... O Homem de Ferro! (som de estalo)"); 
+                Console.ReadLine();
+                    break;
+
+                case '3':
+                Console.WriteLine($"Ligando para Batman...");
+                Thread.Sleep(1250);
+                Console.WriteLine($"I am The Batman, ou Bruce Wayne, quem fala ?"); 
+                Console.ReadLine();
+                    break;
+
+                case '0':
+                this.Desligar();
+                    break;
+
+                default:
+                Console.WriteLine($"Opcao invalida, digite novamente!");
+                Thread.Sleep(1250);
+                    break;
+            }
+
+        } while (EscolhaLigacao != '0');
+            
+        }
+
+        public void EnviarMensagem(){
+        do
+        {
+            Console.WriteLine
+            (@$"
+            Para quem gostaria de Enviar a mensagem ?
+            [1] - Goku;
+            [2] - Iron man;
+            [3] - Batman; 
+
+            [0] - Desligar Celular;           
+            ");
+            EscolhaMensagem = char.Parse(Console.ReadLine());
+            switch (EscolhaMensagem)
+            {
+                case '1':
+                Console.WriteLine($"Escreva sua mensagem...");
+                Console.ReadLine();
+                Thread.Sleep(1250);
+                Console.WriteLine($"Oi, Eu sou o Goku! Mas nao posso falar agora, desculpe!"); 
+                Console.ReadLine();              
+                    break;
+
+                case '2':
+                Console.WriteLine($"Escreva sua mensagem...");
+                Console.ReadLine();
+                Thread.Sleep(1250);
+                Console.WriteLine($"E eu sou... O Homem de Ferro! (som de estalo)... Mas nao posso falar agora, desculpe!"); 
+                Console.ReadLine();
+                    break;
+
+                case '3':
+                Console.WriteLine($"Escreva sua mensagem...");
+                Console.ReadLine();
+                Thread.Sleep(1250);
+                Console.WriteLine($"I am The Batman, ou Bruce Wayne, quem fala ?"); 
+                Console.ReadLine();
+                    break;
+
+                case '0':
+                this.Desligar();
+                    break;
+
+                default:
+                Console.WriteLine($"Opcao invalida, digite novamente!");
+                Thread.Sleep(1250);
+                    break;
+            }
+
+        } while (EscolhaMensagem != '0');
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using aulaPOOCelular;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Celulares celular = new Celulares();
+
+        Console.WriteLine($"Ola, qual a cor do celular achado ?");
+        celular.Cor = Console.ReadLine();
+
+        Console.WriteLine($"E qual o modelo ?");
+        celular.Modelo = Console.ReadLine();
+
+        Console.WriteLine($"Qual o tamanho ? (P/M/G)");
+        celular.Tamanho = char.Parse(Console.ReadLine());
+
+        do
+        {
+            Console.WriteLine($"O celular esta Desligado ? (S/N)");
+            celular.OpcaoDesligado = char.Parse(Console.ReadLine());
+            if (celular.OpcaoDesligado == 'n')
+            {
+                do
+                {
+                    
+                    Console.WriteLine($"No que gostaria de mexer ?");
+                    Console.WriteLine
+                    (@$"
+                    [1] - Fazer ligacao;
+                    [2] - Mandar mensagens;
+                    [3] - Entrar no banco;
+
+                    [0] - Desligar
+                    ");
+                    celular.Funcionalidades = char.Parse(Console.ReadLine());
+                    
+                    switch (celular.Funcionalidades)
+                    {
+                        case '1':
+                        celular.FazerLigacao();
+                            break;
+
+                        case '2':
+                        celular.EnviarMensagem();
+                            break;
+
+                        case '3':
+                        Console.WriteLine($"Vai roubar nada teu ordinario!");
+                        Thread.Sleep(1500);                   
+                            break;
+
+                        case '0':
+                        celular.Desligar();
+                            break;
+                        default:
+                            break;
+                    }
+                } while (celular.Funcionalidades != '0');
+
+            }
+            else
+            {
+                Console.WriteLine($"Consegue ligar o celular ?");
+                celular.EscolhaLigar = char.Parse(Console.ReadLine());
+                if (true)
+                {
+                    
+                }
+                
+            }
+        } while (celular.OpcaoDesligado != 's' || celular.OpcaoDesligado != 'n');
+        
+        
+        
+        
+    }
+}
